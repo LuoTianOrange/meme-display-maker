3f0b5adec57c3ae70ec979ede981aaf4c8187069
xuxiaofen2 <50109347+xuxiaofen2@users.noreply.github.com>
2024-12-26 22:58:19 +0800

    提交PR
    修复删除时无法清空input已选择的文件

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
+import {ref,defineExpose} from 'vue'
+import type { HTMLAttributes} from 'vue'
 import { cn } from '@/lib/utils'
 import { useVModel } from '@vueuse/core'
 
@@ -12,25 +13,20 @@ const props = defineProps<{
 const emits = defineEmits<{
   (e: 'update:modelValue', payload: File[] | null): void
 }>()
-
+// input html引用
+const input = ref<HTMLInputElement>()
+defineExpose({input})
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
   defaultValue: props.defaultValue,
 })
 
-const handleFileChange = (event: Event) => {
-  const target = event.target as HTMLInputElement
-  const files = target.files ? Array.from(target.files) : null
-  console.log('Uploaded files:', files)
-  emits('update:modelValue', files)
-}
 </script>
 
 <template>
   <input
-    type="file"
-    multiple
-    @change="handleFileChange"
+    ref="input"
+    v-model="modelValue"
     :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', props.class)"
   >
 </template>
